c36b4d5dd818f2813a3f94407f5b3d4dc4970492
Craig Buckler <craig@optimalworks.net>
2025-09-17 15:30:01 +0100

    email link re-enabled

--- src/js/lib/email.js
+++ src/js/lib/email.js
@@ -7,14 +7,8 @@ Array.from( document.getElementsByClassName('email') ).forEach(e => {
     es = em.replace(/\sdot\s/ig, '.').replace(/\{at\}/ig,'@').replace(/\s/g,'');
 
   if (em !== es) {
-    const link = e.closest('a');
-    link.href = 'ma'+'ilt'+'o:'+es;
+    e.closest('a').href = 'ma'+'ilt'+'o:'+es;
     if (!ds) e.textContent = es;
-
-    link.addEventListener('click', ev => {
-      alert('Sorry - our email is temporarily unavailable.\nPlease call us on +44 (0)1397 712 324.');
-      ev.preventDefault();
-    });
   }
 
 });
